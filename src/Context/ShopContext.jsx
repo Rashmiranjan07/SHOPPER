3e60a333cacb58db1b7710f6ca723e84e812a123
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-07-13 05:31:57 +0530

    shopper

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useState } from "react";
 import all_product from "../Components/Assets/all_product";
 
 
